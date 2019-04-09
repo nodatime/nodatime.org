f55ff9d76ad38e72a453a29f81e0a0bd352a893e
Jon Skeet <skeet@pobox.com>
2019-04-09 19:19:22 +0100

    Use NodaTime.sln instead of NodaTime-All.sln
    Fixes #5

--- build/testsnippets.sh
+++ build/testsnippets.sh
@@ -4,4 +4,4 @@ rm -rf tmp/snippet_test
 mkdir -p tmp/snippet_test
 
 dotnet publish ../../nodatime/src/NodaTime.Demo
-dotnet run -p SnippetExtractor -- ../../nodatime/src/NodaTime-All.sln NodaTime.Demo tmp/snippet_test
+dotnet run -p SnippetExtractor -- ../../nodatime/src/NodaTime.sln NodaTime.Demo tmp/snippet_test
