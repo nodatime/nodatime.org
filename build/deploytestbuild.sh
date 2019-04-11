bbc9f38519b8561c6380769d8a39a22cb9cea1a6
Jon Skeet <skeet@pobox.com>
2019-04-11 19:10:12 +0100

    Fixes to test deployment

--- build/deploytestbuild.sh
+++ build/deploytestbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 ROOT=$(git rev-parse --show-toplevel)
 (cd $ROOT/src/NodaTime.Web;
  dotnet publish -c Debug;
  echo "Built at $(date -u -Iseconds)" > bin/Debug/netcoreapp2.2/publish/wwwroot/build.txt;
  cp $ROOT/build/deployment/Dockerfile bin/Debug/netcoreapp2.2/publish;
  gcloud.cmd builds submit \
-   --config=$ROOT/build/nodatime.org/testcloudbuild.yaml \
+   --config=$ROOT/build/deployment/testcloudbuild.yaml \
    $ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish)
