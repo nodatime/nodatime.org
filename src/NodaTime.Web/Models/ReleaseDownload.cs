abd47c24980832c3cffc5675046b506d0ded030f
Jon Skeet <skeet@pobox.com>
2019-04-13 10:39:06 +0100

    More minor tidy-ups

--- src/NodaTime.Web/Models/ReleaseDownload.cs
+++ src/NodaTime.Web/Models/ReleaseDownload.cs
@@ -2,8 +2,6 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using System;
-
 namespace NodaTime.Web.Models
 {
     public class ReleaseDownload
--- src/NodaTime.Web/Models/TzdbDownload.cs
+++ src/NodaTime.Web/Models/TzdbDownload.cs
@@ -5,7 +5,6 @@
 using NodaTime.Web.Services;
 using System;
 using System.IO;
-using System.Net.Http;
 using System.Threading;
 
 namespace NodaTime.Web.Models
