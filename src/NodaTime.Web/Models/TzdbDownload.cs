402df5db6f3298390c4c943e6316ee4cf1a5b778
Jon Skeet <skeet@pobox.com>
2019-04-13 10:37:57 +0100

    Add IStorageRepository implementation for local development
    There are still a few hard-coded URLs in place, but *almost* everything works locally
    
    (Notable TODO: when generating URLs for tzdb/index.txt and tzdb/latest.txt, we could use the hosting URL)

--- src/NodaTime.Web/Models/TzdbDownload.cs
+++ src/NodaTime.Web/Models/TzdbDownload.cs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
+using NodaTime.Web.Services;
 using System;
 using System.IO;
 using System.Net.Http;
@@ -14,19 +15,23 @@ namespace NodaTime.Web.Models
     /// </summary>
     public class TzdbDownload
     {
-        private static readonly HttpClient httpClient = new HttpClient();
         public string Name { get; }
         public string NodaTimeOrgUrl { get; }
 
         private readonly Lazy<byte[]> data;
 
-        public TzdbDownload(string storageUrl)
+        public TzdbDownload(IStorageRepository storage, string name)
         {
-            Name = Path.GetFileName(new Uri(storageUrl).LocalPath);
+            Name = Path.GetFileName(name);
             NodaTimeOrgUrl = $"https://nodatime.org/tzdb/{Name}";
-            data = new Lazy<byte[]>(
-                () => httpClient.GetByteArrayAsync(storageUrl).Result,
-                LazyThreadSafetyMode.ExecutionAndPublication);
+            data = new Lazy<byte[]>(LoadContent, LazyThreadSafetyMode.ExecutionAndPublication);
+
+            byte[] LoadContent()
+            {
+                var stream = new MemoryStream();
+                storage.DownloadObject(name, stream);
+                return stream.ToArray();
+            }
         }
 
         public Stream GetContent() => new MemoryStream(data.Value);
