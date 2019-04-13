677579c1af44789120607e93ace6e9a128b4de95
Jon Skeet <skeet@pobox.com>
2019-04-13 14:40:17 +0100

    Trim gRPC native libraries after publishing

--- build/deploytestbuild.sh
+++ build/deploytestbuild.sh
@@ -5,8 +5,13 @@ set -e
 ROOT=$(git rev-parse --show-toplevel)
 (cd $ROOT/src/NodaTime.Web;
  dotnet publish -c Debug;
- echo "Built at $(date -u -Iseconds)" > bin/Debug/netcoreapp2.2/publish/wwwroot/build.txt;
- cp $ROOT/build/deployment/Dockerfile bin/Debug/netcoreapp2.2/publish;
- gcloud.cmd builds submit \
-   --config=$ROOT/build/deployment/testcloudbuild.yaml \
-   $ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish)
+ declare -r PUBLISH=$ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish
+ 
+ # Remove the Windows and OSX gRPC binaries; replace the unused Linux one
+ # with an empty file. (It has to be present, but can be empty.)
+ rm -rf $PUBLISH/runtimes/{osx,win}
+ > $PUBLISH/runtimes/linux/native/libgrpc_csharp_ext.x86.so
+ 
+ echo "Built at $(date -u -Iseconds)" > $PUBLISH/wwwroot/build.txt;
+ cp $ROOT/build/deployment/Dockerfile $PUBLISH;
+ gcloud.cmd builds submit --config=$ROOT/build/deployment/testcloudbuild.yaml $PUBLISH)
