07a6fbf9b199c99b27c47ca77182f778e5c03ce0
Jon Skeet <skeet@pobox.com>
2019-04-16 21:46:53 +0100

    Sort TOCs when combining them
    Fixes #36.

---
+++ build/TocCombiner/Program.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.DocAsCode.Common;
+using Microsoft.DocAsCode.DataContracts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TocCombiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<TocItemViewModel>();
+            foreach (var file in args)
+            {
+                var toc = YamlUtility.Deserialize<TocViewModel>(file);
+                items.AddRange(toc);
+            }
+            var combined = new TocViewModel();
+            combined.AddRange(items.OrderBy(item => item.Name));
+            YamlUtility.Serialize(Console.Out, combined, "YamlMime:TableOfContent");
+        }
+    }
+}
