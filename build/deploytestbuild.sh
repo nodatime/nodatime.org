bc1b11df667b26c9a06aae25cef9c4b915a646f7
Jon Skeet <skeet@pobox.com>
2019-04-30 09:58:30 +0100

    Remove gRPC trimming now we're not using it

--- build/deploytestbuild.sh
+++ build/deploytestbuild.sh
@@ -4,13 +4,9 @@ set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 (cd $ROOT/src/NodaTime.Web;
+ declare -r PUBLISH=$ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish;
+ rm -rf $PUBLISH;
  dotnet publish -c Debug;
- declare -r PUBLISH=$ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish
- 
- # Remove the Windows and OSX gRPC binaries; replace the unused Linux one
- # with an empty file. (It has to be present, but can be empty.)
- rm -rf $PUBLISH/runtimes/{osx,win}
- > $PUBLISH/runtimes/linux/native/libgrpc_csharp_ext.x86.so
  
  echo "Built at $(date -u -Iseconds)" > $PUBLISH/wwwroot/build.txt;
  cp $ROOT/build/deployment/Dockerfile $PUBLISH;
