c126a032e71c673340715871264a1ca64f5c3a62
Jon Skeet <skeet@pobox.com>
2019-04-30 09:58:16 +0100

    Replace Google.Cloud.Diagnostics.AspNetCore with a custom console logger
    This has advantages of:
    
    - No gRPC dependency, which reduces the size significantly
    - No need to detect the environment; we just assume that there's an agent handling stdout appropriately

--- src/NodaTime.Web/Program.cs
+++ src/NodaTime.Web/Program.cs
@@ -5,6 +5,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using NodaTime.Web.Logging;
 
 namespace NodaTime.Web
 {
@@ -18,10 +19,7 @@ namespace NodaTime.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                // Clear all logging here; we'll add back console and debug later. This unfortunately
-                // makes startup harder to diagnose :(
-                // We should probably revisit this. (How much of the configuration is available here in the context?)
-                .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureLogging(logging => logging.AddProvider(new JsonConsoleLoggerProvider()))
                 // Uncomment these lines if startup is failing
                 //.CaptureStartupErrors(true)
                 //.UseSetting("detailedErrors", "true")
