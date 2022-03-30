780484e79d34d02cc2c18336868caacc329a627b
Jon Skeet <skeet@pobox.com>
2022-03-30 11:36:28 +0100

    Temporarily disable snippets entirely
    Better to have a working website without snippets than one which
    can't be updated...

--- build/buildapidocs.sh
+++ build/buildapidocs.sh
@@ -62,8 +62,9 @@ dotnet run --project ReleaseDiffGenerator -- tmp/metadata/NodaTime
 dotnet run --project DocfxAnnotationGenerator -- tmp/metadata/*
 
 # Extract snippets from NodaTime.Demo (unstable only, for now)
-dotnet publish -v quiet ../../nodatime/src/NodaTime.Demo
-dotnet run --project SnippetExtractor -- ../../nodatime/src/NodaTime.sln NodaTime.Demo tmp/metadata/NodaTime/unstable/overwrite
+# Note: snippets are currently disabled until we can get them working again...
+# dotnet publish -v quiet ../../nodatime/src/NodaTime.Demo
+# dotnet run --project SnippetExtractor -- ../../nodatime/src/NodaTime.sln NodaTime.Demo tmp/metadata/NodaTime/unstable/overwrite
 
 # Reorganize the files to suit docfx build
 # NodaTime and NodaTime.Testing
