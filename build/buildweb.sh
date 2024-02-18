851213bf0bd014317e0cc211c212a4eaab785b15
Jon Skeet <skeet@pobox.com>
2024-02-18 16:15:24 +0000

    Start of a new "build everything from scratch" Dockerfile
    We may end up with a copy of this in each repo - or we could do a
    scheduled build (which is effectively what we have now).
    
    The intention is to move the complete docs build into the Cloud.

--- build/buildweb.sh
+++ build/buildweb.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+ROOT=$(git rev-parse --show-toplevel)
+cd $ROOT/build
+
 # Note: use --skip-api to skip building API docs
 
 # Disable msbuild node reuse, in an attempt to stabilize the build.
@@ -13,19 +16,31 @@ export MSBUILDDISABLENODEREUSE=1
 if [[ "$1" != "--skip-api" ]]
 then
   ./buildapidocs.sh
-  rm -rf ../src/NodaTime.Web/docfx
-  cp -r tmp/site ../src/NodaTime.Web/docfx
+  rm -rf $ROOT/src/NodaTime.Web/docfx
+  cp -r tmp/site $ROOT/src/NodaTime.Web/docfx
 fi
 
 # Build the web site ASP.NET Core
-rm -rf ../src/NodaTime.Web/bin/Release
+rm -rf $ROOT/src/NodaTime.Web/bin/Release
 # Make sure minification happens before publish...
-dotnet build -v quiet -c Release ../src/NodaTime.Web
-dotnet publish -v quiet -c Release ../src/NodaTime.Web
+dotnet build -v quiet -c Release $ROOT/src/NodaTime.Web
+dotnet publish -v quiet -c Release $ROOT/src/NodaTime.Web
 
 # Fix up blazor.config to work in Unix
 # (Blazor is currently disabled.)
 # sed -i 's/\\/\//g' $WEB_DIR/NodaTime.Web.Blazor.blazor.config
 
 # Run a smoke test to check it still works
-dotnet test ../src/NodaTime.Web.SmokeTest
+# dotnet test ../src/NodaTime.Web.SmokeTest
+
+# Add diagnostic text files
+declare -r publish=$ROOT/src/NodaTime.Web/bin/Release/net8.0/publish;
+echo "Built at $(date -u -Iseconds)" > $publish/wwwroot/build.txt;
+
+declare -r nodatime_org_commit=$(git -C $ROOT rev-parse HEAD)
+declare -r nodatime_commit=$(git -C $ROOT/../nodatime rev-parse HEAD)
+declare -r nodatime_serialization_commit=$(git -C $ROOT/../nodatime.serialization rev-parse HEAD)
+echo "Commits:" > $publish/wwwroot/commit.txt
+echo "nodatime: $nodatime_commit" >> $publish/wwwroot/commit.txt
+echo "nodatime.org: $nodatime_org_commit" >> $publish/wwwroot/commit.txt
+echo "nodatime.serialization: $nodatime_serialization_commit" >> $publish/wwwroot/commit.txt
