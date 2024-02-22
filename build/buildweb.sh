f86a80fa6392769bc357a4e5b0339a502063483b
Jon Skeet <skeet@pobox.com>
2024-02-22 07:40:00 +0000

    Avoid printing a summary after each build/pack operation
    docfx itself still prints a summary, and it looks like there's no way of avoiding this.

--- build/buildweb.sh
+++ build/buildweb.sh
@@ -23,8 +23,8 @@ fi
 # Build the web site ASP.NET Core
 rm -rf $ROOT/src/NodaTime.Web/bin/Release
 # Make sure minification happens before publish...
-dotnet build -v quiet -c Release $ROOT/src/NodaTime.Web
-dotnet publish -v quiet -c Release $ROOT/src/NodaTime.Web
+dotnet build -nologo -clp:NoSummary -v quiet -c Release $ROOT/src/NodaTime.Web
+dotnet publish -nologo -clp:NoSummary -v quiet -c Release $ROOT/src/NodaTime.Web
 
 # Fix up blazor.config to work in Unix
 # (Blazor is currently disabled.)
