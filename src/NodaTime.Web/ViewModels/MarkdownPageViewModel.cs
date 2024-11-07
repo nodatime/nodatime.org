f7499957fd857366fd01bbb6eec93a884a07d41b
Jon Skeet <skeet@pobox.com>
2024-11-07 18:13:24 +0000

    Remove Try .NET from web site
    Closes #100
    
    We don't have the time to get this working again, really.

--- src/NodaTime.Web/ViewModels/MarkdownPageViewModel.cs
+++ src/NodaTime.Web/ViewModels/MarkdownPageViewModel.cs
@@ -2,18 +2,16 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using Microsoft.AspNetCore.Html;
 using NodaTime.Web.Models;
 
 namespace NodaTime.Web.ViewModels
 {
     public class MarkdownPageViewModel
     {
-        public IHtmlContent TryDotNetSrc { get; }
         public string HostOrigin { get; }
         public MarkdownPage Page { get; }
 
-        public MarkdownPageViewModel(IHtmlContent tryDotNetSrc, string hostOrigin, MarkdownPage page) =>
-            (TryDotNetSrc, HostOrigin, Page) = (tryDotNetSrc, hostOrigin, page);
+        public MarkdownPageViewModel(string hostOrigin, MarkdownPage page) =>
+            (HostOrigin, Page) = (hostOrigin, page);
     }
 }
